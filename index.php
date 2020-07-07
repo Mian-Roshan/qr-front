65ecaa4b2f2617b727efdc2d13b398d2977550d0
danishay <67322647+danishay@users.noreply.github.com>
2020-07-07 14:46:35 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta autor="Adroit Light Solutions">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.scss">
+    <title>Showcase</title>
+</head>
+<body>
+    <div class="main-wrapper">
+        <div class="layer" style="background-image: url(assets/images/first-layer.png);">
+            <div class="nav d-flex ml-auto mr-auto">
+                <a href="#"><img src="assets/images/logo.png"></a>
+                <div class="nav-links ml-auto">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Demo</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="banner ml-auto mr-auto row">
+                <div class="col-md-6 m-0 p-0">
+                    <h1 class="banner-heading">RESTURANT<br>QR WEB MENU</h1>
+                    <p class="banner-p">Add an online menu maker to your resturant website and present your dishes tastly.</p>
+                    <p class="banner-p2">Use our restaurant menu script to create countless categories (salads, soups, meat, desserts etc.) Add multiple dishes to each of them. The menu will blend perfectly into your website with minimum efforts. Personalize every entry by adding name, image and description. Set portion sizes and respective prices. Enable/disable products as per availability. Make your customers happy by launching special offers for a number of people with products of your choice.</p>
+                    <div class="text-right">
+                        <button class="banner-button">Create My Menu</button>
+                    </div>
+                </div>    
+                <div class="col-md-6 laptop">
+                    <img src="assets/images/laptop.png">
+                </div>
+            </div>
+        </div>
+        <div class="main ml-auto mr-auto position-relative">
+            <h1 class="main-heading text-center">Product Highlights</h1>
+            <p class="text-center main-p">The Restaurant QR Web Menu will help you create a mouth-watering online menu with all your<br>dishes and drinks. Adding categories, products and special offers and personalizing them is an easy<br>and convenient job you are going to enjoy. Check the key features below:</p>
+        <div class="row p-0 m-0">
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card1.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Categories & Products</h3>
+                    <p class="text-center products-cards-p">Create multiple menu categories and products. Add names, images and descriptions. Set different portion sizes and the corresponding prices. Enable/disable products depending on their availability.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card2.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Responsive & Varied Design</h3>
+                    <p class="text-center products-cards-p">The restaurant script front-end is mobile-friendly and can be loaded across various devices (PC, smartphones, tablets). You can also change color themes - 10 different are available!</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card3.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Special Offers</h3>
+                    <p class="text-center products-cards-p">The restaurant script front-end is mobile-friendly and can be loaded across various devices (PC, smartphones, tablets). You can also change color themes - 10 different are available!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card4.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Multiple Languages</h3>
+                    <p class="text-center products-cards-p">The online menu maker can be translated into various languages. You can do it from the back-end system or by exporting and importing a CSV file with all system titles.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card5.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Portiojn Size</h3>
+                    <p class="text-center products-cards-p">Enable different portion sizes (e.g. small / medium / family pizza) or allow one dish size only. Set separate prices for each portion size. Easily add and remove sizes from the back-end as necessary.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card6.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Users Roles</h3>
+                    <p class="text-center products-cards-p">There are two types of restaurant script users. Admins have full access rights - they can change system settings and add users. Editors have access to all menus, but restricted rights.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="col-md-4">
+                <div class="products-cards position-relative">
+                    <div class="text-center products-cards-img">
+                        <img src="assets/images/card7.svg" width="90px" height="90px">
+                    </div>
+                    <h3 class="text-center products-cards-h">Cross-domain Integration</h3>
+                    <p class="text-center products-cards-p">You can install the restaurant menu script on domainA and use it on domainB. This means you can embed the software on an external website and thus attract more customers.</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="sec-layer">
+            <img src="assets/images/sec-layer.png" class="img-fluid sec-layer">
+            <div class="row m-0 p-0">
+                <div class="col-md-12">
+                <div class="d-flex feature-and-button">
+                    <h1 class="feature-first-h">Features</h1>
+                    <button class="ml-auto feature-button">Create My Menu</button>
+                </div>
+                </div>
+            </div>
+            <div class="row m-0 p-0">
+                <div class="col-md-6">
+                    <div class="features">
+                        <h3 class="feature-h">EASY MENU MANAGEMENT</h3>
+                        <p class="feature-p">Restaurant Menu Maker has a user-friendly back-end system allowing you to organize and style your menu with pleasure. Specially built for non-technical users, the web based admin panel provides remote access to the restaurant script, so that you can manage your online business both from the office and on the go.</p>
+                        <div class="grey-text">
+                            <p>Create your main and subcategories. Order them, add images and descriptions.</p>
+                            <p>Include unlimited products (dishes) into each category. Add catchy names and galleries.</p>
+                            <p>Set different portion sizes or one dish size only. Each portion goes with a separate price.</p>
+                            <p>Change product status - enable/disable dishes depending on their availability.</p>
+                            <p>Related scripts: Check our <a href="#">Food Delivery Script</a> and <a href="#">table booking system</a>, too!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 pr-0">
+                    <div class="feature1">
+                        <img src="assets/images/feature1.png" class="w-100 feature1 img">    
+                    <div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="row feature-row">
+            <div class="col-md-6 p-0">
+                <div>
+                    <img src="assets/images/feature2.png" class="feature-left-img w-100">
+                </div>
+            </div>
+            <div class="col-md-6 feature-right-text">
+                <div>
+                    <div class="feature-right-opacity-text">
+                        <h1 class="feature-right-opacity-texth">Features</h1>
+                    </div>
+                    <h3 class="feature-h text-right right-border">SPECIAL COMBO MEALS</h3>
+                    <p class="feature-p text-right">The QR Web Menu gives you the opportunity to launch special offers - standard, seasonal, exclusive, whatever you need! It's a good way to attract new customers, win regular ones and boost sales. You can manage all special offers from a separate system menu.</p>
+                    <div class="grey-text text-right">
+                        <p>Add a special offer with an attractive name, short description and catchy image.</p>
+                        <p>Select the products you want to include into each special offer.</p>
+                        <p>Set individual prices for each dish or a total promo price.</p>
+                        <p>Define the number of people the special offer shall apply to.</p>
+                        <p>Enable/disable the offer without deleting it. You will only change its status.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="row p-0 m-0 feature-row">
+        <div class="col-md-6 feature-left-text">
+            <div>
+                <div class="feature-left-opacity-text">
+                    <h1 class="feature-left-opacity-texth">Features</h1>
+                </div>
+                <h3 class="feature-h">HIGH PERFORMANCE & FLEXIBILITY</h3>
+                <p class="feature-p">Just like all other PHPJabbers scripts, the restaurant menu script is developed to ensure an optimal user experience for both customers and admins. It's quick, mobile-friendly and highly customizable.</p>
+                <div class="grey-text">
+                    <p>Responsiveness - The restaurant script is mobile-friendly and loads on various devices.</p>
+                    <p>Colors - You can choose from 10 different color themes and customize your menu.</p>
+                    <p>Compatibility - Integrate online menu maker into all kinds of websites. <a href=#>Watch a video...<a></p>
+                    <p>Fast load time - AJAX is used to refresh the restaurant script only, not the entire web page.</p>
+                    <p>Custom changes - <a href="#">Request a modification</a> to adapt Restaurant Menu Maker to your needs.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/images/feature3.png" class="w-100 feature-right-img">
+            </div>     
+        </div>
+    </div>
+    <div class="row p-0 m-0 feature-row">
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/images/feature4.png" class="feature-left-img w-100">
+            </div>
+        </div>
+        <div class="col-md-6 feature-right-text">
+            <div>
+                <div class="feature-right-opacity-text">
+                    <h1 class="feature-right-opacity-texth">Features</h1>
+                </div>
+                <h3 class="feature-h text-right right-border">SYSTEM SETTING</h3>
+                <p class="feature-p text-right">Configure the restaurant menu script with no coding skills. Add languages, grant access to other users, create internal and external notifications. It takes a mouse click to protect your data from potential loss or misuse.</p>
+                <div class="grey-text text-right">
+                    <p>Multi-language support - Translate all system messages into multiple languages.</p>
+                    <p>User roles - Admins have full access rights and can add other users.</p>
+                    <p>Editors can only view menus.</p>
+                    <p>System Backup - Make a safe copy of all your files and database entries with a single click.</p>
+                    <p>One admin - Control multiple PHPJabbers scripts from a <a href="#">centralized admin area.</a></p>
+               </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="row p-0 m-0 feature-row">
+        <div class="col-md-6 feature-left-text">
+            <div>
+                <div class="feature-left-opacity-text">
+                    <h1 class="feature-left-opacity-texth">Features</h1>
+                </div>
+                <h3 class="feature-h">INSTALLATION & INTEGRATION</h3>
+                <p class="feature-p">An installation script will automatically install the restaurant menu software for you. Just upload the files we will send you on your hosting account and open the installation page. Then add the code on the web page where you want the online restaurant menu to appear.</p>
+                <div class="grey-text">
+                    <p>The Install Wizard will guide you through the installation process. Watch video...</p>
+                    <p>Integrate the online menu maker - Just copy/paste a few lines on your web page.</p>
+                    <p>Cross-domain integration - You can install the restaurant script on domainA and use it 
+                    on domainB.</p>
+                    <p>Installation help - Contact us to request our assistance and we will spare you all the fuzz!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/images/feature5.png" class="w-100 feature-right-img">
+            </div>     
+        </div>
+    </div>
+    <div class="row p-0 m-0 feature-row">
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/images/feature6.png" class="feature-left-img w-100">
+            </div>
+        </div>
+        <div class="col-md-6 feature-right-text">
+            <div>
+                <div class="feature-right-opacity-text">
+                    <h1 class="feature-right-opacity-texth">Features</h1>
+                </div>
+                <h3 class="feature-h text-right right-border">MORE KEY SOFTWARE FEATURES</h3>
+                <p class="feature-p text-right">Just like most of our PHP scripts, Restaurant Menu Maker comes with the following special features & extra services. <a href="#">Find out more…</a></p>
+                <div class="grey-text text-right">
+                    <p>Installation Wizard & Cross Domain Integration</p>
+                    <p>High Performance & Protected Against SQL Injections PHP Framework</p>
+                    <p>One admiN Module, Translation Module & Backup Tool</p>
+                    <p>User Permissions & Encrypted Passwords</p>
+                    <p>Installation assistance, Support & Updates</p>
+                    <p>Payment Gateways Integration, Remote Hosting & Web SMS</p>
+                    <p>Developer Licence & Extended Licence Program</p>
+               </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="benifits">
+        <div class="position-relative">
+            <img class="w-100" src="assets/images/benifits-img.png">
+            <div class="row mx-0  layer-overlay-container h-100 position-absolute w-100">
+                <div class="col text-center">
+                    <img src="assets/images/benifit-1.png">
+                    <h3 class="benifit-h">PAYMENT GATEWAYS</h3>
+                    <p class="benifit-p">Review and request other online payment processing options!</p>
+                </div>
+                <div class="col text-center">
+                    <img src="assets/images/benifit-2.png">
+                    <h3 class="benifit-h">SCRIPT MODIFICATIONS</h3>
+                    <p class="benifit-p">See a detailed introduction of our custom modification service!</p>
+                </div>
+                <div class="col text-center">
+                    <img src="assets/images/benifit-3.png">
+                    <h3 class="benifit-h">HIGH PERFOMANCE</h3>
+                    <p class="benifit-p">Enjoy maximum efficiency and speed with our PHP Framework!</p>
+                </div>
+                <div class="col text-center">
+                    <img src="assets/images/benifit-4.png">
+                    <h3 class="benifit-h">REMOTE HOSTING</h3>
+                    <p class="benifit-p">Save yourself the hassle - let us host our PHP scripts in the cloud!</p>
+                </div>
+                <div class="col text-center">
+                    <img src="assets/images/benifit-5.png">
+                    <h3 class="benifit-h">EXTENDED LICENCE</h3>
+                    <p class="benifit-p">Apply now to promote and resell our PHP scripts on your website!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <p class="text text-center">VIEW ALL BENIFITS</p>
+        <div class="text-center">
+            <img src="assets/images/arrow.png">
+        </div>
+    </div>
+    </div>    
+</body>
+</html>
